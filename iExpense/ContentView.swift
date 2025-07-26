e010a99b1c2ce3655c9575b01b62c8effd7cc01f
aajuvale <ahmed.juvale@gmail.com>
2025-07-26 08:55:33 -0700

    feat: implemented on Delete functionality

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,31 +7,39 @@
 
 import SwiftUI
 
-struct SecondView: View {
-
-    @Environment(\.dismiss)
-    var dismiss
-
-    let name: String
-
-    var body: some View {
-        Button("Dismiss") {
-            dismiss()
-        }
-    }
-}
-
 struct ContentView: View {
     @State
-    private var showingSheet = false
+    private var numbers = [Int]()
+
+    @State
+    private var currentNumber = 1
 
     var body: some View {
-        Button("Show Sheet") {
-            showingSheet.toggle()
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+
+                    Button("Add Number") {
+                        numbers.append(currentNumber)
+                        currentNumber += 1
+                    }
+                }
+
+            }
+            .navigationTitle("onDelete")
+            .toolbar {
+                EditButton()
+            }
         }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "Scott")
-        }
+
+    }
+
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
