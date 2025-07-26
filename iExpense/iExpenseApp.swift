38476e7ce617e60be254f404f05d0c917c4af6ed
aajuvale <ahmed.juvale@gmail.com>
2025-07-25 19:03:51 -0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Ahmed Juvale on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
