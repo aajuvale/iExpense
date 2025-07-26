687a3581e6496abe3bd9bd77d3cb0151d7a16277
aajuvale <ahmed.juvale@gmail.com>
2025-07-26 08:47:08 -0700

    feat: implemented overlaying views

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -5,29 +5,33 @@
 //  Created by Ahmed Juvale on 7/25/25.
 //
 
-import Combine
 import SwiftUI
 
-class User: ObservableObject {
-    @Published
-    var firstName = "Scott"
+struct SecondView: View {
 
-    @Published
-    var lastName = "Pilgrim"
+    @Environment(\.dismiss)
+    var dismiss
+
+    let name: String
+
+    var body: some View {
+        Button("Dismiss") {
+            dismiss()
+        }
+    }
 }
 
 struct ContentView: View {
-    @StateObject
-    var user = User()
+    @State
+    private var showingSheet = false
 
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName)")
-
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
+        Button("Show Sheet") {
+            showingSheet.toggle()
         }
-        .padding()
+        .sheet(isPresented: $showingSheet) {
+            SecondView(name: "Scott")
+        }
     }
 }
 
